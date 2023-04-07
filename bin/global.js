d7a5414a24515e8fed1f950b9c0b4eadbcb95946
Kevin Santiago <sonicthe1111@gmail.com>
2023-04-06 22:38:00 -0600

    Mensajes de consola mas amigables - Readme actualizado

--- bin/global.js
+++ bin/global.js
@@ -4,3 +4,16 @@ const { addScripts } = require('../src/copyConfig.js');
 
 webpackerize();
 addScripts();
+
+process.on('exit', () => {
+  console.log('\n')
+  console.log('\n')
+  console.group('🚀 Webpack configuration completed successfully!');
+  console.log('All files 📝 and directories 📁 have been created✅.');
+  console.log('The following scripts are now available:\n');
+  console.log('  - npm run build: builds the project for production\n');
+  console.log('  - npm run dev: builds the project for development\n');
+  console.log('  - npm start: starts a dev server with hot reloading enabled\n');
+  console.log('Happy coding! 😎\n');
+  console.groupEnd();
+});
--- src/index.js
+++ src/index.js
@@ -4,6 +4,27 @@ const { exec } = require('child_process');
 
 const webpackerize = async () => {
   try {
+    const path = require('path');
+    const fs = require('fs');
+
+    const emojis = {
+      copied: '📝',
+      created: '📁',
+      successfully: '✅',
+      dependencies: '📦',
+      install: '⬇️',
+      stdout: '🆗',
+      stderr: '🚫',
+    };
+
+    function replaceWithEmojis(message) {
+      Object.keys(emojis).forEach((key) => {
+        const regex = new RegExp(key, 'gi');
+        message = message.replace(regex, emojis[key]);
+      });
+      return message;
+    }
+
     const configFilesPath = path.join(__dirname, 'configFiles');
     const configPath = process.cwd();
 
@@ -13,14 +34,16 @@ const webpackerize = async () => {
       const newFilePath = path.join(configPath, fileName);
 
       fs.writeFileSync(newFilePath, fileContent, 'utf8');
-      console.log(`File ${fileName} copied successfully to ${configPath}`);
+      const message = replaceWithEmojis(`File ${fileName} copied successfully to ${path.basename(configPath)}`);
+      console.log(message);
     });
 
     const directoriesToCreate = ['public', 'src', 'src/assets', 'src/styles'];
 
     directoriesToCreate.forEach((directory) => {
       fs.mkdirSync(directory);
-      console.log(`Directory ${directory} created successfully.`);
+      const message = replaceWithEmojis(`Directory ${directory} created successfully.`);
+      console.log(message);
     });
 
     // Lista de archivos a crear
@@ -36,18 +59,20 @@ const webpackerize = async () => {
       const filePath = path.join(process.cwd(), file.path);
       fs.writeFile(filePath, file.content, (err) => {
         if (err) throw err;
-        console.log(`File ${filePath} created successfully.`);
+        const message = replaceWithEmojis(`File ${path.basename(filePath)} created successfully.`);
+        console.log(message);
       });
     });
 
-// Install dependencies
+    // Install dependencies
     const dependencies = ['webpack-dev-server', '@babel/core', '@babel/plugin-transform-runtime', '@babel/preset-env', 'autoprefixer', 'babel-loader', 'copy-webpack-plugin', 'css-loader', 'css-minimizer-webpack-plugin', 'dotenv-webpack', 'html-webpack-plugin', 'mini-css-extract-plugin', 'postcss', 'postcss-loader', 'tailwindcss', 'terser-webpack-plugin', 'webpack', 'webpack-cli'];
 
-    console.log(`Installing dependencies: ${dependencies.join(' ')}`);
+    console.log(replaceWithEmojis(`Installing ${emojis.dependencies}: ${dependencies.join(' ')}`));
 
     const installProcess = exec(`npm install ${dependencies.join(' ')} --save-dev`);
-    installProcess.stdout.on('data', (data) => console.log(data));
-    installProcess.stderr.on('data', (data) => console.error(data));
+    installProcess.stdout.on('data', (data) => console.log(`${replaceWithEmojis(emojis.stdout)} ${data}`));
+    installProcess.stderr.on('data', (data) => console.error(`${replaceWithEmojis(emojis.stderr)} ${data}`));
+    installProcess.on('exit', () => console.log(`${replaceWithEmojis(emojis.install)} Dependencies installed successfully.`));
 
   } catch (error) {
     console.error('An error occurred while executing Webpackerize:', error.message);
